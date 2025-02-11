a910cde832fed63e15268d546e3b19426849f92a
BaxPhoto <mail@baxp.de>
2025-02-11 10:05:18 +0100

    initial load

---
+++ New Panorama2.js
@@ -0,0 +1,43 @@
+PanoramaStudioViewerParams = {
+"panoramaStudioViewer": {
+"key": "",
+"control": {
+        "mousemode": "drag",
+        "touchmode": "drag"
+    },
+"data": {
+        "id": "globalData",
+        "messageBoxStyle": "min-width: 25%; text-shadow: none; line-height: 100%; color:#ffffff; font-family: Arial,Helvetica,Sans-Serif; font-size: 15px; font-weight: 100; text-align:left; box-shadow: 4px 4px 1px 0px rgba(0,0,0,0.33);background: rgba(0,0,0,0.66);"
+    },
+"include": [
+        {
+            "src": "New Panorama2_controls.js"
+        }
+    ],
+"node": {
+	"autoplay": {
+	        "pan": 0.08,
+	        "reference": "screen",
+	        "restart": 5,
+	        "returnToLevel": true,
+	        "status": "off",
+	        "tilt": 0,
+	        "zoom": 1
+	    },
+	"camera": {
+	        "maxpan": 360,
+	        "maxtilt": 33.620867062310275,
+	        "minpan": 0,
+	        "mintilt": -29.77810065746739
+	    },
+	"id": "New Panorama2",
+	"image":{"baseindex":1,"bitmap":[{"back":{"src":"New Panorama2_tiles/New Panorama2_t_1b_%0y_%0x.jpg"},"down":{"src":"New Panorama2_tiles/New Panorama2_t_1d_%0y_%0x.jpg"},"front":{"src":"New Panorama2_tiles/New Panorama2_t_1f_%0y_%0x.jpg"},"height":8523,"left":{"src":"New Panorama2_tiles/New Panorama2_t_1l_%0y_%0x.jpg"},"right":{"src":"New Panorama2_tiles/New Panorama2_t_1r_%0y_%0x.jpg"},"tilesize":533,"up":{"src":"New Panorama2_tiles/New Panorama2_t_1u_%0y_%0x.jpg"},"width":8523},{"back":{"src":"New Panorama2_tiles/New Panorama2_t_2b_%0y_%0x.jpg"},"down":{"src":"New Panorama2_tiles/New Panorama2_t_2d_%0y_%0x.jpg"},"front":{"src":"New Panorama2_tiles/New Panorama2_t_2f_%0y_%0x.jpg"},"height":4262,"left":{"src":"New Panorama2_tiles/New Panorama2_t_2l_%0y_%0x.jpg"},"right":{"src":"New Panorama2_tiles/New Panorama2_t_2r_%0y_%0x.jpg"},"tilesize":533,"up":{"src":"New Panorama2_tiles/New Panorama2_t_2u_%0y_%0x.jpg"},"width":4262},{"back":{"src":"New Panorama2_tiles/New Panorama2_t_3b_%0y_%0x.jpg"},"down":{"src":"New Panorama2_tiles/New Panorama2_t_3d_%0y_%0x.jpg"},"front":{"src":"New Panorama2_tiles/New Panorama2_t_3f_%0y_%0x.jpg"},"height":2131,"left":{"src":"New Panorama2_tiles/New Panorama2_t_3l_%0y_%0x.jpg"},"right":{"src":"New Panorama2_tiles/New Panorama2_t_3r_%0y_%0x.jpg"},"tilesize":533,"up":{"src":"New Panorama2_tiles/New Panorama2_t_3u_%0y_%0x.jpg"},"width":2131},{"back":{"src":"New Panorama2_tiles/New Panorama2_t_4b_%0y_%0x.jpg"},"down":{"src":"New Panorama2_tiles/New Panorama2_t_4d_%0y_%0x.jpg"},"front":{"src":"New Panorama2_tiles/New Panorama2_t_4f_%0y_%0x.jpg"},"height":1066,"left":{"src":"New Panorama2_tiles/New Panorama2_t_4l_%0y_%0x.jpg"},"right":{"src":"New Panorama2_tiles/New Panorama2_t_4r_%0y_%0x.jpg"},"tilesize":533,"up":{"src":"New Panorama2_tiles/New Panorama2_t_4u_%0y_%0x.jpg"},"width":1066},{"back":{"src":"New Panorama2_tiles/New Panorama2_t_5b_%0y_%0x.jpg"},"down":{"src":"New Panorama2_tiles/New Panorama2_t_5d_%0y_%0x.jpg"},"front":{"src":"New Panorama2_tiles/New Panorama2_t_5f_%0y_%0x.jpg"},"height":533,"left":{"src":"New Panorama2_tiles/New Panorama2_t_5l_%0y_%0x.jpg"},"right":{"src":"New Panorama2_tiles/New Panorama2_t_5r_%0y_%0x.jpg"},"tilesize":533,"up":{"src":"New Panorama2_tiles/New Panorama2_t_5u_%0y_%0x.jpg"},"width":533}],"multilevel":true,"preview":{"src":"New Panorama2_tiles/New Panorama2_preview.jpg"},"projection":"cubic"},
+	"view": {
+	        "hfov": 90,
+	        "pan": 180,
+	        "tilt": 0
+	    }
+},
+"version": "4.2"
+}
+}
